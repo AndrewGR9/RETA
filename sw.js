4e82800928a348df18d16a7da0e9e9f0f7585e27
AndrewGR9 <andres.gonzalez.rivera92@gmail.com>
2026-09-07 10:59:48 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='reta-v2';
-const FILES=['./','./index.html','./manifest.json'];
+const CACHE='reta-v3';
+const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
