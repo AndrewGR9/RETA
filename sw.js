ad70ce34a7ecaee70379e6236175cfefb84fec8f
AndrewGR9 <andres.gonzalez.rivera92@gmail.com>
2026-09-07 09:44:08 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='reta-v2';
+const FILES=['./','./index.html','./manifest.json'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
